f6d1ba7942926a468e11e3a1c23f13d2a1ac8ab3
kylin <kylinsoong.1214@gmail.com>
2025-03-12 18:05:54 +0800

    add plugin

---
+++ agent/bot_pdf_read.py
@@ -0,0 +1,18 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://ark.cn-beijing.volces.com/api/v3/bots",
+    api_key=os.environ.get("ARK_API_KEY")
+)
+
+completion = client.chat.completions.create(
+    model=os.environ.get("ARK_API_BOT_ID"),  
+    messages=[
+        #{"role": "system", "content": "内容概要"},
+        {"role": "user", "content": "https://pub-kylin.tos-cn-beijing.volces.com/0003/sample.pdf"},
+    ],
+)
+
+
+print(completion.choices[0].message.content)
