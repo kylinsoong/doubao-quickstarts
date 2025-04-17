f374dfb03d2ebbb4b292f0fe750de8bb8c19a2fb
kylin <kylinsoong.1214@gmail.com>
2025-04-17 21:44:23 +0800

    WIP: temporary save before rebase

--- capabilities/qa/test_audio_length.py
+++ capabilities/qa/test_audio_length.py
@@ -17,7 +17,6 @@ def get_wav_file_length(file_path):
     return None
 
 if __name__ == "__main__":
-    file_path = 'https://sec-kylin.tos-cn-beijing.volces.com/juzi/202503201603-a3692a3e-e8ad-4719-83ab-518f417dae92-.wav?X-Tos-Algorithm=TOS4-HMAC-SHA256&X-Tos-Credential=AKLTZTE0Y2QxZmZhYWI1NDZkYmFiNGY5M2FhYzk2NTk1MmM%2F20250416%2Fcn-beijing%2Ftos%2Frequest&X-Tos-Date=20250416T114658Z&X-Tos-Expires=3600&X-Tos-SignedHeaders=host&X-Tos-Signature=18d0e9a24c71101a6e43cb9e0125ea8f0d686654e9bea5d637ed60f68b48fa2a'
     length = get_wav_file_length(file_path)
     if length is not None:
         print(f"音频文件时长: {length} 秒")    
