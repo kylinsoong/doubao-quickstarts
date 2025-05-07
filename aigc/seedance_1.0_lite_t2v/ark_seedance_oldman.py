b7b16c183086c21a84f470378f811d2dbc9f317d
kylin <kylinsoong.1214@gmail.com>
2025-05-08 07:55:16 +0800

    update cganges

---
+++ aigc/seedance_1.0_lite_t2v/ark_seedance_oldman.py
@@ -0,0 +1,40 @@
+import os
+import time  
+from volcenginesdkarkruntime import Ark
+
+client = Ark(
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+    api_key=os.environ.get("ARK_API_KEY"),
+)
+
+if __name__ == "__main__":
+    print("----- create request -----")
+    create_result = client.content_generation.tasks.create(
+        model=os.environ.get("ARK_API_ENGPOINT_ID"),
+        content=[
+            {
+                # 文本提示词
+                "type": "text",
+                "text": "戴着帽子的老爷爷面带微笑往前走"
+            },
+        ]
+    )
+    print(create_result)
+
+    # 轮询查询部分
+    print("----- pooling task status -----")
+    task_id = create_result.id
+    while True:
+        get_result = client.content_generation.tasks.get(task_id=task_id)
+        status = get_result.status
+        if status == "succeeded":
+            print("----- task succeeded -----")
+            print(get_result)
+            break
+        elif status == "failed":
+            print("----- task failed -----")
+            print(f"Error: {get_result.error}")
+            break
+        else:
+            print(f"Current status: {status}, Retrying after 10 seconds...")
+            time.sleep(10)
