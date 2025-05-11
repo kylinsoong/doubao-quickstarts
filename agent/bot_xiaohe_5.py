eea97b3e030ba89bcb78811c7e8e85cb0a02dd7a
kylin <kylinsoong.1214@gmail.com>
2025-05-11 19:31:21 +0800

    update changes

---
+++ agent/bot_xiaohe_5.py
@@ -0,0 +1,32 @@
+import os
+from volcenginesdkarkruntime import Ark
+from urllib.parse import urlparse, urlunparse
+
+def remove_url_parameters(url):
+    """
+    移除 URL 中的所有查询参数和 fragment，只保留 scheme + netloc + path
+    """
+    parsed = urlparse(url)
+    cleaned = parsed._replace(query="", fragment="")  # 去掉 ? 和 #
+    return urlunparse(cleaned)
+
+client = Ark(
+    api_key = os.environ.get("ARK_API_KEY"),  #ARK_API_KEY 需要替换为您在平台创建的 API Key
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+)
+
+prompt = "过度运动可能有害健康"
+
+completion = client.bot_chat.completions.create(
+    model="bot-20250403175147-xdvqj",
+    messages = [
+        {"role": "user", "content": prompt},
+    ],
+)
+
+print(prompt)
+print()
+#print(completion.choices[0].message.content)
+print()
+for r in completion.references:
+    print(remove_url_parameters(r.url))
