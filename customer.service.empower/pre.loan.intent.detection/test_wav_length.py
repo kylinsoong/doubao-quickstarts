8a22b02c31a96ae5f84b5216b7fa067f758f0f34
kylin <kylinsoong.1214@gmail.com>
2025-08-03 18:16:13 +0800

    update changes

--- customer.service.empower/pre.loan.intent.detection/test_wav_length.py
+++ customer.service.empower/pre.loan.intent.detection/test_wav_length.py
@@ -3,7 +3,7 @@ import wave
 import contextlib
 import io
 
-url = "https://haier-poc.tos-cn-shanghai.volces.com/haier/881311_20250618100738066.wav?X-Tos-Algorithm=TOS4-HMAC-SHA256&X-Tos-Credential=AKLTYWNjMDE4OTAyNzBmNGE1Nzg3YjEzMmFiY2UxMmViYTY%2F20250803%2Fcn-shanghai%2Ftos%2Frequest&X-Tos-Date=20250803T021847Z&X-Tos-Expires=3600&X-Tos-SignedHeaders=host&X-Tos-Signature=1f4f65427e3085bd666774c8e42f4338f04e479e35a48e3caddacdb60fb46b34"
+url = "https://haier-poc.tos-cn-shanghai.volces.com/haier/881311_20250618100738066.wav"
 
 response = requests.get(url, stream=True)
 response.raise_for_status()  # Ensure the request was successful
